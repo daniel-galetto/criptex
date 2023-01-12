6c2b9973655d0af57f1b5ab3a4ba45074e59e368
daniel-galetto <daniel.galetto.arg@gmail.com>
2023-01-11 21:47:23 -0300

    add function ocultar and add gif

--- js/script.js
+++ js/script.js
@@ -1,28 +1,23 @@
 function codificar() {
-    mensaje = document.getElementById("texto").value
+    mensaje = document.getElementById("texto").value.toLowerCase();
     mensajeCodificado = [];
     for (let indice = 0; indice < mensaje.length; indice++) {
         codificado = mensaje[indice].replace("e","enter").replace("i", "imes").replace("a","ai").replace("o","ober").replace("u", "ufat");
         mensajeCodificado += codificado;
     }
-    document.getElementById("msg").innerHTML = mensajeCodificado;
-    document.getElementById("texto").value = ""
 
-    document.getElementById("msg").style.display = "block";
-    document.getElementById("container").style.display = "none";
-    document.getElementById("boton-copiar").style.display = "inline";
+    document.getElementById("msg").innerHTML = mensajeCodificado;
+    
+    ocultar();
 }
 
 
 function decodificar(){
-    mensaje = document.getElementById("texto").value
+    mensaje = document.getElementById("texto").value.toLowerCase();
     mensajeDecodificado = mensaje.replace(/enter/g,"e").replace(/imes/g,"i").replace(/ai/g,"a").replace(/ober/g,"o").replace(/ufat/g,"u");
     document.getElementById("msg").innerHTML = mensajeDecodificado;
-    document.getElementById("texto").value = ""
-
-    document.getElementById("msg").style.display = "block";
-    document.getElementById("container").style.display = "none";
-    document.getElementById("boton-copiar").style.display = "inline";
+    
+   ocultar();
 } 
 
 
@@ -48,3 +43,10 @@ function copiar(){
     })
  
 }
+
+function ocultar(){
+    document.getElementById("texto").value = ""
+    document.getElementById("msg").style.display = "block";
+    document.getElementById("container").style.display = "none";
+    document.getElementById("boton-copiar").style.display = "inline";
+} 
